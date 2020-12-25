168715d1977cd81d9a2d48a5bcf1d95f0f014eba
Tanmya Vishvakarma <tanmya2000@gmail.com>
2020-12-25 18:07:43 +0530

    update logo/navbar

--- src/components/dashboard/Navbar.js
+++ src/components/dashboard/Navbar.js
@@ -3,28 +3,28 @@ import React from 'react'
 import { Link } from "react-router-dom";
 import {Icon} from 'react-materialize';
 import './navbar.css'
-import Logo from '../assets/pts.jpg'
+import Logo from '../assets/logo2.png'
 class Navbar extends Component{
     render(){
         return(
             <div>
                 <nav>
-                    <div className="nav-wrapper bold">
+                    <div className="nav-wrapper white">
                         <Link to="/"><div className="logo left"><img className="logoimg"src={Logo} width="64px"></img></div></Link>
                         <span className="sidenav-trigger hide-on-large-only right" data-target="slide-out"><Icon>menu</Icon></span>
                             <ul id="nav-mobile" className="right hide-on-med-and-down">
-                                <li><Link className="links white-text text-darken-3 center-align" to="/findJob">Home</Link></li>
-                                <li><Link className="white-text text-darken-3" to="/postJob">About</Link></li>
-                                <li><Link className="white-text text-darken-3" to="/login">Events</Link></li>
-                                <li><Link className="white-text text-darken-3"  to="/register">Contact</Link></li>
+                                <li><Link className="black-text text-darken-3 center-align" to="/">Home</Link></li>
+                                <li><Link className="black-text text-darken-3" to="/about">About</Link></li>
+                                <li><Link className="black-text text-darken-3" to="/events">Events</Link></li>
+                                <li><Link className="black-text text-darken-3"  to="/contact">Contact</Link></li>
                             </ul>
                     </div>
                 </nav>
-                <ul className="sidenav grey" id="slide-out">
-                <li><Link className="sidenav-close white-text" to="/findJob">Home</Link></li>
-            <li><Link className="sidenav-close white-text " to="/postJob">About</Link></li>
-            <li><Link className="sidenav-close white-text" to="/login">Events</Link></li>
-            <li><Link className="sidenav-close white-text" to="/register">Register</Link></li>
+                <ul className="sidenav white" id="slide-out">
+                <li><Link className="sidenav-close black-text" to="/">Home</Link></li>
+            <li><Link className="sidenav-close black-text " to="/about">About</Link></li>
+            <li><Link className="sidenav-close black-text" to="/events">Events</Link></li>
+            <li><Link className="sidenav-close black-text" to="/contact">Contact</Link></li>
           
                 </ul>
         </div>
