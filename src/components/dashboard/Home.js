ba234ed81af604f766186ba153d42c3ce8bc93ed
Tanmya Vishvakarma <tanmya2000@gmail.com>
2020-12-27 21:14:28 +0530

    update home page

--- src/components/dashboard/Home.js
+++ src/components/dashboard/Home.js
@@ -2,29 +2,70 @@ import React from 'react'
 import {Component} from 'react'
 import Bgimg from '../assets/back.jpg'
 import './home.css'
+import Particles from 'react-particles-js';
+
+
 class Home extends Component{
+    
     render(){
         return (
             <div className="hcontainer">
-                <div className="arrow-wapper">
-                    <div className="arrow1"></div>
-                    <div className="arrow1"></div>
-                    <div className="arrow1"></div>
-                    <div className="arrow1"></div>
-                    <div className="arrow"></div>
-                    <div className="arrow"></div>
-                    <div className="arrow"></div>  
-                </div>
-                <div className="bgimg-holder">
-                    <div className="toptext">
-                        PLATFORM TECH SOLUTIONS
+                <div className="text-wrapper">
+                    <div className="scale-in-center" id="welcome">
+                        Your Imagination, Our Creation
                     </div>
-                    <div className="subtext">
-                        Your Imagination, Our Creations
+                    <div className="scale-in-center" id="welcome2">
+                    PLATFORM TECH SOLUTION
                     </div>
-                 
-                </div>
               
+              
+                </div>
+                
+               <Particles id="particles-js"
+               
+                 params={{
+                      particles: {
+                          number: {
+                              value: 90,
+                              density: {
+                                  enable: true,
+                                  value_area: 700
+                              }
+                          },
+                          color: {
+                              value: '#fff'
+                          },
+                          opacity: {
+                              value: 1,
+                              random:true,
+                              anim: {
+                                  enable: true
+                              }
+                          },
+                          size: {
+                              value: 4,
+                              random: true,
+                              anim: {
+                                  enable: true,
+                                  speed: 3
+                              }
+                          },
+                          line_linked: {
+                              enable: true
+                          },
+                          move: {
+                              speed: 5
+                          }
+                      },
+          
+                  }}
+                  
+             
+                  
+                />
+
+
+            <footer></footer>
             </div>
         
         );
--- src/components/dashboard/Navbar.js
+++ src/components/dashboard/Navbar.js
@@ -3,29 +3,35 @@ import React from 'react'
 import { Link } from "react-router-dom";
 import {Icon} from 'react-materialize';
 import './navbar.css'
-import Logo from '../assets/logo2.png'
+import Logo from '../assets/ptslogo.png'
 class Navbar extends Component{
     render(){
         return(
-            <div>
+            <div className="navbar-fixed"> 
                 <nav>
-                    <div className="nav-wrapper white">
-                        <Link to="/"><div className="logo left"><img className="logoimg"src={Logo} width="64px"></img></div></Link>
+                    <div className="nav-wrapper black">
+                        <Link to="/"><div className="logo left"><img className="logoimg"src={Logo} width="64px" alt="Platform Tech Solutions"></img></div></Link>
                         <span className="sidenav-trigger hide-on-large-only right" data-target="slide-out"><Icon>menu</Icon></span>
                             <ul id="nav-mobile" className="right hide-on-med-and-down">
-                                <li><Link className="black-text text-darken-3 center-align" to="/">Home</Link></li>
-                                <li><Link className="black-text text-darken-3" to="/about">About</Link></li>
-                                <li><Link className="black-text text-darken-3" to="/events">Events</Link></li>
-                                <li><Link className="black-text text-darken-3"  to="/contact">Contact</Link></li>
+                                <li><Link className="white-text  center-align" to="/">Home</Link></li>
+                                <li><Link className="white-text " to="/about">About</Link></li>
+                                <li><Link className="white-text"  to="/ourwork">Our Work</Link></li>
+                                <li><Link className="white-text" to="/events">Events</Link></li>
+                                <li><Link className="white-text"  to="/contact">Register</Link></li>
+                                <li><Link className="white-text"  to="/contact">Login</Link></li>
+                                <li><Link className="white-text"  to="/contact">Contact</Link></li>
+                            
                             </ul>
                     </div>
                 </nav>
-                <ul className="sidenav white" id="slide-out">
-                <li><Link className="sidenav-close black-text" to="/">Home</Link></li>
-            <li><Link className="sidenav-close black-text " to="/about">About</Link></li>
-            <li><Link className="sidenav-close black-text" to="/events">Events</Link></li>
-            <li><Link className="sidenav-close black-text" to="/contact">Contact</Link></li>
-          
+                <ul className="sidenav black" id="slide-out">
+                    <li><Link className="sidenav-close white-text" to="/">Home</Link></li>
+                    <li><Link className="sidenav-close white-text " to="/about">About</Link></li>
+                    <li><Link className="sidenav-close white-text" to="/ourwork">Our Work</Link></li>
+                    <li><Link className="sidenav-close white-text" to="/events">Events</Link></li>
+                    <li><Link className="sidenav-close white-text" to="/contact">Register</Link></li>
+                    <li><Link className="sidenav-close white-text" to="/contact">Login</Link></li>
+                    <li><Link className="sidenav-close white-text" to="/contact">Contact</Link></li>
                 </ul>
         </div>
         );
